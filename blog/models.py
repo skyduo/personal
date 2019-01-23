505f78c8aad5ee7b108bb6435b4bf30d271759d9
skyduo <htd19941208@gmail.com>
2019-01-22 17:28:39 -0800

    美化了Blog页面
    Blog主页只显示少量正文

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,16 @@ class Blog(models.Model):
 	date = models.DateField()
 	image = models.ImageField(default='default.png', upload_to='images/')
 	text = models.TextField(default='正文')
+
 	def __str__(self):
 		return self.title
+
+	def short_text(self):
+		j=100
+		for i in range(100,0,-1):
+			if self.text[i] !=' ':
+				continue
+			else:
+				j=i
+				break
+		return self.text[:j] +'...'
